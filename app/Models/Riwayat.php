bc9e7f3dbc1f1d7043fadf9e77a22ec0be68bfe1
mubar3 <khusnul.mubar@yahoo.com>
2023-05-25 10:40:13 +0700

    Initial commit

---
+++ app/Models/Riwayat.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Riwayat extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'nama',
+        'hasil_diagnosa',
+        'cf_max',
+        'status_user',
+        'status_role',
+        'gejala_terpilih',
+        'file_pdf',
+        'user_id',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function penyakit()
+    {
+        return $this->belongsTo(Penyakit::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
